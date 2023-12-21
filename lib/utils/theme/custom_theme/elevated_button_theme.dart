f4e0846fe992bc4a5f8efd186b5be3ea1f724ecd
rohitarora <rohitarora@extensioncrm.com>
2023-12-21 07:10:19 +0530

    first commit

---
+++ lib/utils/theme/custom_theme/elevated_button_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class TElevatedButtonTheme{
+  TElevatedButtonTheme._();
+
+  static ElevatedButtonThemeData lightelevatedbuttontheme=ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      side:const BorderSide(color: Colors.blue),
+      padding: EdgeInsets.symmetric(vertical: 18),
+      textStyle: TextStyle(fontSize: 16,color: Colors.white,fontWeight: FontWeight.bold),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))
+    )
+  );
+  static ElevatedButtonThemeData darkelevatedbuttontheme=ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+          elevation: 0,
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blue,
+          disabledForegroundColor: Colors.grey,
+          disabledBackgroundColor: Colors.grey,
+          side:const BorderSide(color: Colors.blue),
+          padding: EdgeInsets.symmetric(vertical: 18),
+          textStyle: TextStyle(fontSize: 16,color: Colors.white,fontWeight: FontWeight.bold),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))
+      )
+  );
+}
